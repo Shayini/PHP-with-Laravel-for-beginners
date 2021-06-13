812d508784a780060fb5e6e4f67893e9644c3011
Thakshajini Suhumar <tsuhumar8@gmail.com>
2021-06-14 00:55:48 +0530

    Displaying and Deleting Permissions

---
+++ 16_newapplication/16_new-cms-system/resources/views/admin/permissions/edit.blade.php
@@ -0,0 +1,5 @@
+<x-admin-master>
+    @section('content')
+        Edit: {{$permission->name}}
+    @endsection
+</x-admin-master>
--- 16_newapplication/16_new-cms-system/routes/web/permissions.php
+++ 16_newapplication/16_new-cms-system/routes/web/permissions.php
@@ -1,6 +1,9 @@
 <?php
 
 
-Route::get('/permissions', [App\Http\Controllers\PermissionController::class, 'index'])->name('permissions.index')
+Route::get('/permissions', [App\Http\Controllers\PermissionController::class, 'index'])->name('permissions.index');
+Route::post('/permissions', [App\Http\Controllers\PermissionController::class, 'store'])->name('permissions.store');
+Route::delete('/permissions/{permission}/destroy', [App\Http\Controllers\PermissionController::class, 'destroy'])->name('permissions.destroy');
+Route::get('/permissions/{permission}/edit', [App\Http\Controllers\PermissionController::class, 'edit'])->name('permissions.edit');
 
 ?>
