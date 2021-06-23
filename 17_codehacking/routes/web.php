a113d263c123d1ed38eeea3f3cf0be096b8ab601
Thakshajini Suhumar <tsuhumar8@gmail.com>
2021-06-23 09:53:19 +0530

    Create AdminUsersController and add routes for it and Test the methods

--- 17_codehacking/routes/web.php
+++ 17_codehacking/routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('admin/users', App\Http\Controllers\AdminUsersController::class);
