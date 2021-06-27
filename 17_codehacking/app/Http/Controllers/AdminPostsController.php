1470f05ff2330b065640c344986fc2d5bb6f42a9
Thakshajini Suhumar <tsuhumar8@gmail.com>
2021-06-27 21:34:45 +0530

    Editing Post

--- 17_codehacking/app/Http/Controllers/AdminPostsController.php
+++ 17_codehacking/app/Http/Controllers/AdminPostsController.php
@@ -85,6 +85,11 @@ class AdminPostsController extends Controller
     public function edit($id)
     {
         //
+        $post = Post::findOrFail($id);
+
+        $categories = Category::pluck('name', 'id')->all();
+
+        return view('admin.posts.edit', compact('post', 'categories'));
     }
 
     /**
@@ -97,6 +102,23 @@ class AdminPostsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $input = $request->all();
+
+        $user = Auth::user();
+
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->posts()->whereId($id)->first()->update($input);
+
+        return redirect('/admin/posts');
     }
 
     /**
--- 17_codehacking/resources/views/admin/posts/edit.blade.php
+++ 17_codehacking/resources/views/admin/posts/edit.blade.php
@@ -3,4 +3,41 @@
 
 @section('content')
     <h1>Edit Post</h1>
+
+    <div class="row">
+        {!! Form::model($post, ['method' => 'PATCH', 'action' => ['App\Http\Controllers\AdminPostsController@update', $post->id], 'files' => true]) !!}    <!-- Opening A Form -->
+
+            <div class="form-group">
+                <!-- <input type="text" name="title" placeholder="Enter title"> -->
+                {!! Form::label('title', 'Title:') !!}    <!-- Generating A Label Element, 'title' - value for 'for' attribute, Title - label name -->
+                {!! Form::text('title', null, ['class'=>'form-control']) !!}    <!-- Generating A text Input, title - value for 'name' attribute, default value is null, 'form-control' - value for 'class' attribute -->
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('category_id', 'Category:') !!}
+                {!! Form::select('category_id', $categories, null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('photo_id', 'Photo:') !!}
+                {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('body', 'Description:') !!}
+                {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                <!-- <input type="submit" name="submit"> -->
+                {!! Form::submit('Update Post', ['class'=>'btn btn-primary']) !!}   <!-- Generating A Submit Button, 'Update User - name shown in the button, 'btn btn-primary' - value for 'class' attribute -->
+            </div>
+
+        <!-- </form> -->
+        {!! Form::close() !!}
+    </div>
+
+    <div class="row">
+        @include('includes.form_error')
+    </div>
 @stop
