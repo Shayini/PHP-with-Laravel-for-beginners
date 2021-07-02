d30309b538ff2be0828d5d8d4d8f9b3e355dae68
Thakshajini Suhumar <tsuhumar8@gmail.com>
2021-07-02 12:17:48 +0530

    Displaying Replies

--- 17_newapplication/17_codehacking/resources/views/post.blade.php
+++ 17_newapplication/17_codehacking/resources/views/post.blade.php
@@ -70,32 +70,36 @@
                     </h4>
                     <p>{{$comment->body}}</p>
 
-                    <!-- Nested Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
+                    @if(count($comment->replies) > 0)
+                        @foreach($comment->replies as $reply)
+                            <!-- Nested Comment -->
+                            <div id="nested-comment" class="media">
+                                <a class="pull-left" href="#">
+                                    <img height="64" class="media-object" src="{{$reply->photo}}" alt="">
+                                </a>
+                                <div class="media-body">
+                                    <h4 class="media-heading">{{$reply->author}}
+                                        <small>{{$reply->created_at->diffForHumans()}}/small>
+                                    </h4>
+                                    <p>{{$reply->body}}</p>
+                                </div>
 
 
-                        {!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\CommentRepliesController@createReply']) !!}
-                            <input type="hidden" name="comment_id" value="{{$comment->id}}">
+                                {!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\CommentRepliesController@createReply']) !!}
+                                    <input type="hidden" name="comment_id" value="{{$comment->id}}">
 
-                            <div class="form-group">
-                                {!!  Form::label('body', 'Body:')!!}
-                                {!! Form::textarea('body', null, ['class'=>'form-control', 'rows'=>1]) !!}
+                                    <div class="form-group">
+                                        {!!  Form::label('body', 'Body:')!!}
+                                        {!! Form::textarea('body', null, ['class'=>'form-control', 'rows'=>1]) !!}
+                                    </div>
+                                    <div class="form-group">
+                                        {!! Form::submit('Submit Reply', ['class'=>'btn btn-primary']) !!}
+                                    </div>
+                                {!! Form::close() !!}
                             </div>
-                            <div class="form-group">
-                                {!! Form::submit('Submit Reply', ['class'=>'btn btn-primary']) !!}
-                            </div>
-                        {!! Form::close() !!}
-                    </div>
-                    <!-- End Nested Comment -->
+                            <!-- End Nested Comment -->
+                        @endforeach
+                    @endif
                 </div>
             </div>
         @endforeach
