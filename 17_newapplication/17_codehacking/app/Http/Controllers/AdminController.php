5a352a50250b6fb77fbdc072455d5d1ca96f2153
Thakshajini Suhumar <tsuhumar8@gmail.com>
2021-07-07 12:59:35 +0530

    Installing Chart and Replacing Chart Static Data with Dynamic data

--- 17_newapplication/17_codehacking/app/Http/Controllers/AdminController.php
+++ 17_newapplication/17_codehacking/app/Http/Controllers/AdminController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\Comment;
+
 class AdminController extends Controller
 {
     //
     public function index(){
-        return view('admin.index');
+        $postsCounts = Post::count();
+        $categoriesCounts = Category::count();
+        $commentsCounts = Comment::count();
+
+
+        return view('admin.index', compact('postsCounts', 'categoriesCounts', 'commentsCounts'));
     }
 }
